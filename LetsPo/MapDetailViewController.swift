c9162b99f13d53aff164e1b931b8743d41b1f232
Wen Zi Yu <gage0328@gmail.com>
2017-07-28 11:05:38 +0800

    get new core data from PL

--- LetsPo/MapDetailViewController.swift
+++ LetsPo/MapDetailViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 
 class MapDetailViewController: UIViewController {
+    
+    var dataManagerCount = Int()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        dataManagerCount = boardDataManager.count()
+        
     }
 
     override func didReceiveMemoryWarning() {
