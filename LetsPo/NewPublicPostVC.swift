5c5faa95fd241286336f45709660ee1d732f5b09
Wen Zi Yu <gage0328@gmail.com>
2017-08-08 09:35:00 +0800

    for update

--- LetsPo/NewPublicPostVC.swift
+++ LetsPo/NewPublicPostVC.swift
@@ -21,6 +21,8 @@ class NewPublicPostVC: UIViewController, UINavigationControllerDelegate, UIImage
     var animationsCount = 0
     
 
+    var reUpdate = NSLock()
+    var shouldReUpdate = Bool()
    
     
     @IBOutlet weak var collectionView: UICollectionView!
@@ -114,7 +116,14 @@ class NewPublicPostVC: UIViewController, UINavigationControllerDelegate, UIImage
             self.publicPost.addSubview(self.myTextView)
         }
     }
-//    typealias Completion = (_ success: Bool ) -> ()
+    
+    func doUnlock(){
+        reUpdate.unlock()
+        if shouldReUpdate{
+            shouldReUpdate = false
+        }
+    }
+
     
     // MARK: Perpare segue
     
@@ -123,6 +132,15 @@ class NewPublicPostVC: UIViewController, UINavigationControllerDelegate, UIImage
         
         let dragSegue = segue.destination as! DragPublicPostVC
         
+//        self.doUnlock()
+        
+        
+//        
+//            if reUpdate.try() == false {
+//                shouldReUpdate = true
+//                return
+//            }
+
         
         UIView.animate(withDuration: 0.1) {
             self.myTextView.isEditable = false
@@ -147,7 +165,7 @@ class NewPublicPostVC: UIViewController, UINavigationControllerDelegate, UIImage
         dragSegue.boardID = boardID
         
     }
-    
+        
     
     func setKeyboardObserver() {
         
